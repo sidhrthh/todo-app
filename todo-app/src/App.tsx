0771b0adf4fe3e995ed537ccc0748eea0cafba42
Sidharth pandro <sidharthpandro50@gmail.com>
2024-05-10 01:11:43 +0530

    inital commit & setup

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-center">
+    Hello world!
+  </h1>
+  );
+}
+
+export default App;
